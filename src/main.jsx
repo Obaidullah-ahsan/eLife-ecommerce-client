31b5b51eb6bf5a6c2eae836b65a32526097b02a1
Obaidullah-ahsan <abir60303@gmail.com>
2024-11-25 12:04:51 +0600

    install react hot toast and firebase

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { router } from "./Routes/Routes.jsx";
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="max-w-screen-xl mx-auto">
       <RouterProvider router={router} />
+      <Toaster position="top-right" reverseOrder={false} />
     </div>
   </StrictMode>
 );
