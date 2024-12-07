ce19ba0b023865627d858a94b3bfadf20d93c931
Obaidullah-ahsan <abir60303@gmail.com>
2024-12-07 16:51:47 +0600

    Product add to cart

--- src/pages/Shared/ProductCard.jsx
+++ src/pages/Shared/ProductCard.jsx
@@ -1,7 +1,27 @@
+import axios from "axios";
+import toast from "react-hot-toast";
 import { CiHeart } from "react-icons/ci";
 
 const ProductCard = ({ product }) => {
-  const { product_name, price, image} = product;
+  const { _id, product_name, price, image, product_category, brand } = product;
+  const handleAddCart = (id) => {
+    const cartProduct = {
+      productId: id,
+      product_name,
+      price,
+      image,
+      product_category,
+      brand,
+    };
+    axios.post("http://localhost:5000/cart", cartProduct).then((res) => {
+      if (res.data?.insertedId === null) {
+        toast.error(res.data?.Message);
+      }
+      else{
+        toast.success("Product Added Successfully");
+      }
+    });
+  };
   return (
     <div className="card py-4 bg-white rounded-none card-compact hover:scale-110 ease-in-out duration-300">
       <figure>
@@ -19,7 +39,10 @@ const ProductCard = ({ product }) => {
         <h2 className="md:text-lg font-bold">{product_name}</h2>
         <p className="text-[#F26E21] text-lg font-bold">Tk. {price}</p>
         <div className="flex gap-4">
-          <button className="btn min-h-9 h-9 text-[#F26E21] bg-[#ffdcc8] hover:bg-[#F26E21] hover:text-white rounded-sm">
+          <button
+            onClick={() => handleAddCart(_id)}
+            className="btn min-h-9 h-9 text-[#F26E21] bg-[#ffdcc8] hover:bg-[#F26E21] hover:text-white rounded-sm"
+          >
             Add to Cart
           </button>
         </div>
